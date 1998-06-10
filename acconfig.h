aa996f9499ef264134227e5de80c5efdd6675d0a
Owen Taylor <otaylor@gtk.org>
1998-06-10 15:48:25 +0000

    Check for installed MICO version, and the libs it requires by using
    Wed Jun 10 11:34:52 1998  Owen Taylor  <otaylor@gtk.org>
    
    	* configure.in acconfig.h: Check for installed MICO
    	  version, and the libs it requires by using mico-ld
    	  and mico-setup.sh.
    
    	  Add a HAVE_MICO_ORB_RUN macro to signal that we need
    	  to call orb->run(), as we do for mico-2.0.6 and later
    
    Wed Jun 10 11:41:11 1998  Owen Taylor  <otaylor@gtk.org>
    
    	* */Makefile.am: Use MICO_LIBS variable from
    	  toplevel configure.in.
    
    Wed Jun 10 11:42:16 1998  Owen Taylor  <otaylor@gtk.org>
    
    	* Makefile.am: Use MICO_LIBS variable from
    	  toplevel configure.in.
    
    	* applet-lib.cc launcher-lib.cc mico-lib.cc. Call
    	  orb->run() at appropriate times when HAVE_MICO_ORB_RUN
    	  is defined.

--- acconfig.h
+++ acconfig.h
@@ -1,13 +1,15 @@
 #undef ENABLE_NLS
 #undef HAVE_CATGETS
+#undef HAVE_DEVGTK
 #undef HAVE_GETTEXT
 #undef HAVE_LC_MESSAGES
 #undef HAVE_STPCPY
 #undef HAVE_LIBSM
-#undef PACKAGE
-#undef VERSION
 #undef HAVE_PROGRAM_INVOCATION_SHORT_NAME
 #undef HAVE_PROGRAM_INVOCATION_NAME
+#undef HAVE_MICO_ORB_RUN
+#undef PACKAGE
+#undef VERSION
 
 /* Name of default window manager, as string.  */
 #undef WINDOW_MANAGER
