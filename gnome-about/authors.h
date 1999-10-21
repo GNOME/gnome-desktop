7a67db0688d0f18f01f5d35fa4c9e44b36e3a426
Federico Mena Quintero <federico@redhat.com>
1999-10-21 16:30:26 +0000

    Me me me. Alphabetized as well.
    1999-10-21  Federico Mena Quintero  <federico@redhat.com>
    
    	* authors.h (authors): Me me me.  Alphabetized as well.

--- gnome-about/authors.h
+++ gnome-about/authors.h
@@ -5,14 +5,17 @@ typedef struct {
 	const gchar *email;
 } author;
 
+/* Please keep this in alphabetical order */
 static author authors[] = {
 	{ N_("GNOME was brought to you by"), "" },
 	{ "", "" },
-	{ "Anders Carlsson", "<andersca@gnu.org>" },
 	{ "Jacob Berkman", "<jberkman@andrew.cmu.edu>" },
-	{ "Kjartan Maraas", "<kmaraas@online.no>" },
+	{ "Anders Carlsson", "<andersca@gnu.org>" },
 	{ "George Lebl", "<jirka@5z.com>" },
+	{ "Kjartan Maraas", "<kmaraas@online.no>" },
+	{ "Federico Mena-Quintero", "<federico@gimp.org>" },
+	{ "The Squeaky Rubber Gnome", "http://www.gnome.org/squeaky-rubber-gnome.shtml" },
 	{ "", "" },
-	{ N_("...and many more"), "" },
-	{NULL, NULL}
+	{ N_("... and many more"), "" },
+	{ NULL, NULL }
 };
