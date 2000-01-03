9d9dea7daf58bf377d7276a4e4c7cca7e0804ccd
Kjartan Maraas <kmaraas@online.no>
2000-01-03 20:35:12 +0000

    A little script that lets people update the .pot file without makefiles
    2000-01-03  Kjartan Maraas  <kmaraas@online.no>
    
    	* update.sh: A little script that lets people
    	update the .pot file without makefiles etc.

---
+++ po/update.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+xgettext --default-domain=gnome-core --directory=.. \
+  --add-comments --keyword=_ --keyword=N_ \
+  --files-from=./POTFILES.in \
+&& test ! -f gnome-core.po \
+   || ( rm -f ./gnome-core.pot \
+    && mv gnome-core.po ./gnome-core.pot )
