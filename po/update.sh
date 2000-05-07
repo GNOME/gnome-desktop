0d21e0d094aa406bfd97b8a742054ddf8c008340
Kenneth Christiansen <kenneth@src.gnome.org>
2000-05-07 20:18:31 +0000

    Fixed somethings for portability Thanks to Mathieu for the information
    Kenneth

--- po/update.sh
+++ po/update.sh
@@ -2,7 +2,7 @@
 
 PACKAGE="gnome-core"
 
-if [ -z $1 ]; then
+if [ "x$1" = "x" ]; then 
 
 xgettext --default-domain=$PACKAGE --directory=.. \
   --add-comments --keyword=_ --keyword=N_ \
@@ -19,6 +19,8 @@ xgettext --default-domain=gfloppy --directory=.. \
 && test ! -f $PACKAGE.po \
    || ( rm -f ./PACKAGE.pot \
 && mv $PACKAGE.po ./$PACKAGE.pot );
+
+echo Now merging $1.po with $PACKAGE.pot, and creating an updated $1.po 
 
 mv $1.po $1.po.old && msgmerge $1.po.old $PACKAGE.pot -o $1.po \
 && rm $1.po.old;
