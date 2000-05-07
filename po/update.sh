f6bd080e9bc9dd8b2bedacb99f865e8ac8514f27
Kenneth Christiansen <kenneth@src.gnome.org>
2000-05-07 20:00:24 +0000

    New update.sh
    Kenneth

--- po/update.sh
+++ po/update.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 
-xgettext --default-domain=gnome-core --directory=.. \
+PACKAGE="gnome-core"
+
+if [ -z $1 ]; then
+
+xgettext --default-domain=$PACKAGE --directory=.. \
   --add-comments --keyword=_ --keyword=N_ \
   --files-from=./POTFILES.in \
-&& test ! -f gnome-core.po \
-   || ( rm -f ./gnome-core.pot \
-    && mv gnome-core.po ./gnome-core.pot )
+&& test ! -f $PACKAGE.po \
+   || ( rm -f ./$PACKAGE.pot \
+&& mv $PACKAGE.po ./$PACKAGE.pot );
+
+else
+
+xgettext --default-domain=gfloppy --directory=.. \
+  --add-comments --keyword=_ --keyword=N_ \
+  --files-from=./POTFILES.in \
+&& test ! -f $PACKAGE.po \
+   || ( rm -f ./PACKAGE.pot \
+&& mv $PACKAGE.po ./$PACKAGE.pot );
+
+mv $1.po $1.po.old && msgmerge $1.po.old $PACKAGE.pot -o $1.po \
+&& rm $1.po.old;
+
+fi;
