2a934dcac8bbb34c181fb1aea4cc5669c3598ff4
Kenneth Christiansen <kenneth@src.gnome.org>
2000-05-14 18:52:07 +0000

    Better update.sh script
    Kenneth

--- po/update.sh
+++ po/update.sh
@@ -6,10 +6,34 @@ if [ "x$1" = "x--help" ]; then
 
 echo Usage: ./update.sh langcode
 echo --help                  display this help and exit
+echo --missing	             search for missing files in POTFILES.in
 echo
 echo Examples of use:
 echo ./update.sh ----- just creates a new pot file from the source
 echo ./update.sh da -- created new pot file and updated the da.po file 
+
+elif [ "x$1" = "x--missing" ]; then
+
+echo "Searching for files containing _( ) but missing in POTFILES.in..."
+find ../ -regex '.*\.[c|y|cc|c++|h]' | xargs grep _\( | cut -d: -f1 | uniq | cut -d/ -f2- > POTFILES.in.new
+
+echo Sorting... comparing...
+sort -d POTFILES.in -o POTFILES.in
+sort -d POTFILES.in.new -o POTFILES.in.missing
+
+diff POTFILES.in POTFILES.in.missing -u0 | grep '^+' |grep -v '^+++'|grep -v '^@@' > POTFILES.in.missing
+
+if [ -s POTFILES.in.missing ]; then
+echo && echo "Here are the results:"
+echo && cat POTFILES.in.missing
+echo && echo "File POTFILES.in.missing is being placed in directory..."
+
+else
+
+echo &&echo "There are no missing files, thanks God!"
+rm POTFILES.in.missing
+
+fi 
 
 elif [ "x$1" = "x" ]; then 
 
