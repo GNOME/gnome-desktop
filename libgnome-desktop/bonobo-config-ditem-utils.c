76b8565dff8f9ade7855fc29a09a1f64fe8dda7b
Martin Baulig <baulig@suse.de>
2001-06-08 01:33:35 +0000

    Implemented CORBA_tk_struct.
    2001-06-08  Martin Baulig  <baulig@suse.de>
    
    	* bonobo-config-ditem-utils.c (bonobo_config_ditem_decode_any):
    	Implemented CORBA_tk_struct.

--- libgnome-desktop/bonobo-config-ditem-utils.c
+++ libgnome-desktop/bonobo-config-ditem-utils.c
@@ -22,32 +22,12 @@
 
 #include "bonobo-config-ditem-utils.h"
 
-/* fixme: this is a ORBit function, which should be public */
-static gpointer
-ORBit_demarshal_allocate_mem(CORBA_TypeCode tc, gint nelements)
+static gint
+key_compare_func (gconstpointer a, gconstpointer b)
 {
-	size_t block_size;
-	gpointer retval = NULL;
+	DirEntry *de = (DirEntry *) a;
 
-	if (!nelements)
-		return retval;
-
-	block_size = ORBit_gather_alloc_info (tc);
-
-	if (block_size) {
-		retval = ORBit_alloc_2 
-			(block_size * nelements,
-			 (ORBit_free_childvals) ORBit_free_via_TypeCode,
-			 GINT_TO_POINTER (nelements), sizeof (CORBA_TypeCode));
-
-		*(CORBA_TypeCode *)((char *)retval - 
-				    sizeof(ORBit_mem_info) - 
-				    sizeof(CORBA_TypeCode)) = 
-			(CORBA_TypeCode)CORBA_Object_duplicate
-			((CORBA_Object)tc, NULL);
-	}
-
-	return retval;
+	return strcmp (de->name, b);
 }
 
 CORBA_any *
@@ -58,12 +38,14 @@ bonobo_config_ditem_decode_any (DirEntry *de, CORBA_TypeCode type, CORBA_Environ
 	g_return_val_if_fail (de != NULL, NULL);
 	g_return_val_if_fail (ev != NULL, NULL);
 
+#if 0
 	g_message (G_STRLOC ": |%s| - %d - %s (%s)", de->value,
 		   type->kind, type->name, type->repo_id);
+#endif
 
 	switch (type->kind) {
 	case CORBA_tk_string:
-		any = bonobo_arg_new (TC_string);
+		any = bonobo_arg_new (TC_CORBA_string);
 		BONOBO_ARG_SET_STRING (any, de->value);
 		break;
 
@@ -109,7 +91,12 @@ bonobo_config_ditem_decode_any (DirEntry *de, CORBA_TypeCode type, CORBA_Environ
 
 	}
 
-	case CORBA_tk_struct:
+	case CORBA_tk_struct: {
+		DynamicAny_DynStruct dynstruct;
+		CORBA_NameValuePairSeq *members;
+		gulong length, i;
+		GSList *l;
+
 		if (CORBA_TypeCode_equal (type, TC_GNOME_LocalizedString, NULL)) {
 			GNOME_LocalizedString localized;
 
@@ -119,9 +106,49 @@ bonobo_config_ditem_decode_any (DirEntry *de, CORBA_TypeCode type, CORBA_Environ
 			any = bonobo_arg_new (TC_GNOME_LocalizedString);
 			BONOBO_ARG_SET_GENERAL (any, localized, TC_GNOME_LocalizedString,
 						GNOME_LocalizedString, ev);
+			break;
 		}
 
+		dynstruct = CORBA_ORB_create_dyn_struct (bonobo_orb (), type, ev);
+
+		length = CORBA_TypeCode_member_count (type, ev);
+
+		members = CORBA_sequence_CORBA_NameValuePair__alloc ();
+		members->_length = length;
+		members->_buffer = CORBA_sequence_CORBA_NameValuePair_allocbuf (length);
+
+		for (i = 0; i < length; i++) {
+			CORBA_Identifier member_name;
+			CORBA_TypeCode member_type;
+			CORBA_any *subvalue = NULL;
+			DirEntry *subentry = NULL;
+
+			member_name = CORBA_TypeCode_member_name (type, i, ev);
+			member_type = CORBA_TypeCode_member_type (type, i, ev);
+
+			l = g_slist_find_custom (de->subvalues, member_name, key_compare_func);
+			if (l) {
+				subentry = l->data;
+				subvalue = bonobo_config_ditem_decode_any (subentry, member_type, ev);
+			}
+
+			if (!subvalue)
+				subvalue = bonobo_arg_new (member_type);
+
+			members->_buffer [i].id = CORBA_string_dup (member_name);
+			CORBA_any__copy (&members->_buffer [i].value, subvalue);
+		}
+
+		DynamicAny_DynStruct_set_members (dynstruct, members, ev);
+
+		CORBA_free (members);
+
+		members = DynamicAny_DynStruct_get_members (dynstruct, ev);
+
+		any = DynamicAny_DynAny_to_any (dynstruct, ev);
+
 		break;
+	}
 
 	default:
 		break;
