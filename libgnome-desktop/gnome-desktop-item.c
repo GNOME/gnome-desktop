acb02186e0df163d91464629e0a4fb082f7eae28
Seth Nickell <snickell@stanford.edu>
2001-11-08 12:55:29 +0000

    Stop including gnome-vfs-mime.h, use gnome-vfs-mime-util.h instead.
    2001-11-08  Seth Nickell  <snickell@stanford.edu>
    
    	* gnome-desktop-item.c:
    
    	Stop including gnome-vfs-mime.h, use gnome-vfs-mime-util.h instead.

--- libgnome-desktop/gnome-desktop-item.c
+++ libgnome-desktop/gnome-desktop-item.c
@@ -47,7 +47,7 @@
 #include <locale.h>
 #include <popt.h>
 
-#include <libgnomevfs/gnome-vfs-mime.h>
+#include <libgnomevfs/gnome-vfs-mime-utils.h>
 #include <libgnomevfs/gnome-vfs-uri.h>
 #include <libgnomevfs/gnome-vfs-ops.h>
 #include <libgnomevfs/gnome-vfs-utils.h>
