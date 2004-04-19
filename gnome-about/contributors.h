c6463ddadad66a41d8bf196ad5551c2d22aa1c27
Sergey V. Udaltsov <svu@src.gnome.org>
2004-04-19 23:30:22 +0000

    Adding myself - the longest line in the contributors.h, by far! Thanks to UTF-8 and hexadecimal escaping.

--- gnome-about/contributors.h
+++ gnome-about/contributors.h
@@ -305,6 +305,8 @@ static gchar *contributors[] = {
 	"Malcolm Tredinnick",
 	"Tom Tromey",
 	"Jon Trowbridge",
+	/* Sergey V. Udaltsov */
+	"\xd0\xa1\xd0\xb5\xd1\x80\xd0\xb3\xd0\xb5\xd0\xb9 \xd0\x92. \xd0\xa3\xd0\xb4\xd0\xb0\xd0\xbb\xd1\x8c\xd1\x86\xd0\xbe\xd0\xb2",
 	"Vincent Untz",
 	"Manish Vachharajani",
 	"Neil Vachharajani",
