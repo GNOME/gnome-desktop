ef70b08e033e6c005b8da3f8b367a32ef990f85a
Vincent Untz <vuntz@gnome.org>
2006-05-29 17:09:56 +0000

    Backport from HEAD.
    2006-05-29  Vincent Untz  <vuntz@gnome.org>
    
    	Backport from HEAD.
    
            * gnome-about/gnome-about.c: (get_description_messages),
            (display_version_info): Fix compile warnings and refcount leak.
            Bug #336068.

--- gnome-about/gnome-about.c
+++ gnome-about/gnome-about.c
@@ -665,7 +665,7 @@ get_description_messages (xmlNodePtr node)
 			const GList *t;
 
 			if (paras->type == XML_ELEMENT_NODE &&
-					xmlStrEqual (paras->name, "p") &&
+					xmlStrEqual (paras->name, (const xmlChar *) "p") &&
 					value && value[0]) {
 				cur = xmlNodeGetLang (paras);
 
@@ -682,7 +682,7 @@ get_description_messages (xmlNodePtr node)
 					best_value = g_strdup (tmp);
 				}
 				else {
-					if (!cur || xmlStrEqual (cur, "C")) {
+					if (!cur || xmlStrEqual (cur, (const xmlChar *) "C")) {
 						break;
 					}
 					/* See if the current lanaguage occurs
@@ -796,7 +796,7 @@ display_version_info (GnomeCanvasGroup *group)
 
 	node = about->children;
 
-	if (g_ascii_strcasecmp (node->name, "gnome-version")) {
+	if (g_ascii_strcasecmp ((const char *) node->name, "gnome-version")) {
 		xmlFreeDoc (about);
 		return;
 	}
