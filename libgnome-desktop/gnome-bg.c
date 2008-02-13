5ebdff769e764740f8be69e5fa1c02416506e992
Vincent Untz <vuntz@gnome.org>
2008-02-13 08:17:34 +0000

    don't unref NULL Fix crash #516103. Patch by Joe Marcus Clarke
    2008-02-13  Vincent Untz  <vuntz@gnome.org>
    
    	* gnome-bg.c: (get_mtime): don't unref NULL
    	Fix crash #516103.
    	Patch by Joe Marcus Clarke <marcus@freebsd.org>
    
    svn path=/trunk/; revision=5009

--- libgnome-desktop/gnome-bg.c
+++ libgnome-desktop/gnome-bg.c
@@ -1076,10 +1076,11 @@ get_mtime (const char *uri)
 		file = g_file_new_for_uri (uri);
 		info = g_file_query_info (file, G_FILE_ATTRIBUTE_TIME_MODIFIED,
 					  G_FILE_QUERY_INFO_NONE, NULL, NULL);
-		if (info)
+		if (info) {
 			mtime = g_file_info_get_attribute_uint64 (info,
 								  G_FILE_ATTRIBUTE_TIME_MODIFIED);
-		g_object_unref (info);
+			g_object_unref (info);
+		}
 		g_object_unref (file);
 	}
 	
