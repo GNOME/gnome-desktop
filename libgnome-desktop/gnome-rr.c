4ecbd8c42ae8080bf61d5fa1dea69a67e06f1b5f
Vincent Untz <vuntz@gnome.org>
2010-03-27 12:35:32 +0100

    Move translator comment to the right place
    It wasn't visible to translators.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=610571

--- libgnome-desktop/gnome-rr.c
+++ libgnome-desktop/gnome-rr.c
@@ -439,8 +439,8 @@ fill_out_screen_info (Display *xdisplay,
     }
     else
     {
-        /* Translators: a CRTC is a CRT Controller (this is X terminology). */
 	g_set_error (error, GNOME_RR_ERROR, GNOME_RR_ERROR_RANDR_ERROR,
+		     /* Translators: a CRTC is a CRT Controller (this is X terminology). */
 		     _("could not get the screen resources (CRTCs, outputs, modes)"));
 	return FALSE;
     }
