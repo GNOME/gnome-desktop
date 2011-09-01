fb9ee91145db8904f1baf7c60fbbfaf36dd05e47
Colin Walters <walters@verbum.org>
2011-09-01 11:53:23 -0400

    Add GnomeWallClock
    Since clocks are a bit tricky, it's useful to centralize this class.
    See https://bugzilla.gnome.org/show_bug.cgi?id=655129
    See https://bugzilla.gnome.org/show_bug.cgi?id=657074
    
    https://bugzilla.gnome.org/show_bug.cgi?id=657955

---
+++ libgnome-desktop/tests/test-wallclock.c
@@ -0,0 +1,36 @@
+#define GNOME_DESKTOP_USE_UNSTABLE_API
+#include <libgnome-desktop/gnome-wall-clock.h>
+
+static void
+update_clock (GnomeWallClock *clock)
+{
+  g_print ("%s\n", gnome_wall_clock_get_clock (clock));
+}
+
+static void
+on_clock_changed (GnomeWallClock *clock,
+		  GParamSpec     *pspec,
+		  gpointer        user_data)
+{
+  update_clock (clock);
+}
+
+int
+main (int    argc,
+      char **argv)
+{
+  GnomeWallClock *clock;
+  GMainLoop *loop;
+
+  g_type_init ();
+  
+  loop = g_main_loop_new (NULL, TRUE);
+  
+  clock = g_object_new (GNOME_TYPE_WALL_CLOCK, NULL);
+  g_signal_connect (clock, "notify::clock", G_CALLBACK (on_clock_changed), NULL);
+  update_clock (clock);
+
+  g_main_loop_run (loop);
+
+  return 0;
+}
