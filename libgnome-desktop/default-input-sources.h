4c8b6d90e3f30ca7b186fe4005dd6ee535d3bb6a
Rui Matos <tiagomatos@gmail.com>
2013-01-29 14:53:37 +0100

    gnome-languages: Add default input sources per locale
    This will allow us to have good defaults so that we don't have to
    bother asking the user something that we can derive from the chosen
    locale.
    
    The information in this initial table comes from
    http://bugzilla-attachments.gnome.org/attachment.cgi?id=223428 and
    user provided data in https://live.gnome.org/GnomeGoals/KeyboardData.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=693773

---
+++ libgnome-desktop/default-input-sources.h
@@ -0,0 +1,52 @@
+typedef struct
+{
+  const gchar const *locale;
+  const gchar const *type;
+  const gchar const *id;
+} DefaultInputSource;
+
+static DefaultInputSource default_input_sources[] =
+{
+  { "ar_DZ",    "xkb",          "ara+azerty" },
+  { "as_IN",    "ibus",         "m17n:as:phonetic" },
+  { "ast_ES",   "xkb",          "es+ast" },
+  { "az_AZ",    "xkb",          "az" },
+  { "be_BY",    "xkb",          "by" },
+  { "bn_IN",    "ibus",         "m17n:bn:inscript" },
+  { "cat_ES",   "xkb",          "es+cat" },
+  { "cs_CZ",    "xkb",          "cz" },
+  { "de_DE",    "xkb",          "de" },
+  { "el_CY",    "xkb",          "gr" },
+  { "el_GR",    "xkb",          "gr" },
+  { "en_GB",    "xkb",          "gb" },
+  { "en_US",    "xkb",          "us" },
+  { "es_ES",    "xkb",          "es" },
+  { "fr_FR",    "xkb",          "fr+oss" },
+  { "gl_ES",    "xkb",          "es" },
+  { "gu_IN",    "ibus",         "m17n:gu:inscript" },
+  { "he_IL",    "xkb",          "il" },
+  { "hi_IN",    "ibus",         "m17n:hi:inscript" },
+  { "it_IT",    "xkb",          "it" },
+  { "ja_JP",    "ibus",         "anthy" },
+  { "kn_IN",    "ibus",         "m17n:kn:kgp" },
+  { "ko_KR",    "ibus",         "hangul" },
+  { "mai_IN",   "ibus",         "m17n:mai:inscript" },
+  { "ml_IN",    "ibus",         "m17n:ml:inscript" },
+  { "mr_IN",    "ibus",         "m17n:mr:inscript" },
+  { "nl_NL",    "xkb",          "us+altgr-intl" },
+  { "or_IN",    "ibus",         "m17n:or:inscript" },
+  { "pa_IN",    "ibus",         "m17n:pa:inscript" },
+  { "pl_PL",    "xkb",          "pl" },
+  { "pt_BR",    "xkb",          "br" },
+  { "pt_PT",    "xkb",          "pt" },
+  { "ru_RU",    "xkb",          "ru" },
+  { "sd_IN",    "ibus",         "m17n:sd:inscript" },
+  { "sk_SK",    "xkb",          "sk" },
+  { "ta_IN",    "ibus",         "m17n:ta:tamil99" },
+  { "te_IN",    "ibus",         "m17n:te:inscript" },
+  { "ur_IN",    "ibus",         "m17n:ur:phonetic" },
+  { "zh_CN",    "ibus",         "libpinyin" },
+  { "zh_HK",    "ibus",         "cangjie" },
+  { "zh_TW",    "ibus",         "chewing" },
+  { NULL,       NULL,           NULL }
+};
