3d805a5c3533455946f2564fc5f7d69f21bf71c4
Florian Müllner <fmuellner@gnome.org>
2014-11-12 20:14:38 +0100

    bg-slide-show: Fix annotation

--- libgnome-desktop/gnome-bg-slide-show.c
+++ libgnome-desktop/gnome-bg-slide-show.c
@@ -565,6 +565,7 @@ gnome_bg_slide_show_get_current_slide (GnomeBGSlideShow  *self,
  * @frame_number: frame number
  * @width: monitor width
  * @height: monitor height
+ * @progress: (out) (allow-none): slide progress
  * @duration: (out) (allow-none): slide duration
  * @is_fixed: (out) (allow-none): if slide is fixed
  * @file1: (out) (allow-none) (transfer none): first file in slide
