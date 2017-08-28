36b131c9e8fa848dedf35e58344bae7b93582185
Bastien Nocera <hadess@hadess.net>
2017-08-28 11:35:50 +0200

    thumbnailer: Add copyright notice to test application

--- libgnome-desktop/test-desktop-thumbnail.c
+++ libgnome-desktop/test-desktop-thumbnail.c
@@ -1,3 +1,26 @@
+/*
+ * Copyright (C) 2012,2017 Red Hat, Inc.
+ *
+ * This file is part of the Gnome Library.
+ *
+ * The Gnome Library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * The Gnome Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with the Gnome Library; see the file COPYING.LIB.  If not,
+ * write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ * Authors: Bastien Nocera <hadess@hadess.net>
+ */
+
 #define GNOME_DESKTOP_USE_UNSTABLE_API
 #include <libgnome-desktop/gnome-desktop-thumbnail.h>
 #include <gtk/gtk.h>
