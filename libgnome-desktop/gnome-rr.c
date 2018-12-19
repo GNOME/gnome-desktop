GNOME/gnome-desktop

commit 632dffb7887c8650154670ae0e76822ae11e22f3

API: GnomeRR: replace direct XRandR calls with DBus calls to mutter
Mutter now provides a DBus API that wraps XRandR and abstracts
away the detail of running X or wayland.

A number of API changes are needed, as GnomeRR is no longer in
charge of maintaining the monitors.xml file updated.

https://bugzilla.gnome.org/show_bug.cgi?id=705510

libgnome-desktop/gnome-rr.h
@@ -111,11 +111,6 @@ GnomeRROutput **gnome_rr_screen_list_outputs       (GnomeRRScreen         *scree
 GnomeRRCrtc **  gnome_rr_screen_list_crtcs         (GnomeRRScreen         *screen);
 GnomeRRMode **  gnome_rr_screen_list_modes         (GnomeRRScreen         *screen);
 GnomeRRMode **  gnome_rr_screen_list_clone_modes   (GnomeRRScreen	  *screen);
-void            gnome_rr_screen_set_size           (GnomeRRScreen         *screen,
-						    int                    width,
-						    int                    height,
-						    int                    mm_width,
-						    int                    mm_height);
 GnomeRRCrtc *   gnome_rr_screen_get_crtc_by_id     (GnomeRRScreen         *screen,
 						    guint32                id);
 gboolean        gnome_rr_screen_refresh            (GnomeRRScreen         *screen,
@@ -129,12 +124,6 @@ void            gnome_rr_screen_get_ranges         (GnomeRRScreen         *scree
 						    int                   *max_width,
 						    int                   *min_height,
 						    int                   *max_height);
-void            gnome_rr_screen_get_timestamps     (GnomeRRScreen         *screen,
-						    guint32               *change_timestamp_ret,
-						    guint32               *config_timestamp_ret);
-
-void            gnome_rr_screen_set_primary_output (GnomeRRScreen         *screen,
-                                                    GnomeRROutput         *output);
 
 GnomeRRMode   **gnome_rr_screen_create_clone_modes (GnomeRRScreen *screen);
 
@@ -149,21 +138,12 @@ gboolean        gnome_rr_screen_set_dpms_mode      (GnomeRRScreen         *scree
 guint32         gnome_rr_output_get_id             (GnomeRROutput         *output);
 const char *    gnome_rr_output_get_name           (GnomeRROutput         *output);
 const char *    gnome_rr_output_get_display_name   (GnomeRROutput         *output);
-gboolean        gnome_rr_output_is_connected       (GnomeRROutput         *output);
-int             gnome_rr_output_get_size_inches    (GnomeRROutput         *output);
-int             gnome_rr_output_get_width_mm       (GnomeRROutput         *outout);
-int             gnome_rr_output_get_height_mm      (GnomeRROutput         *output);
-const guint8 *  gnome_rr_output_get_edid_data      (GnomeRROutput         *output,
-                                                    gsize                 *size);
-gboolean        gnome_rr_output_get_ids_from_edid  (GnomeRROutput         *output,
+void            gnome_rr_output_get_ids_from_edid  (GnomeRROutput         *output,
                                                     char                 **vendor,
-                                                    int                   *product,
-                                                    int                   *serial);
+                                                    char                 **product,
+                                                    char                 **serial);
 
-gint            gnome_rr_output_get_backlight_min  (GnomeRROutput         *output);
-gint            gnome_rr_output_get_backlight_max  (GnomeRROutput         *output);
-gint            gnome_rr_output_get_backlight      (GnomeRROutput         *output,
-                                                    GError                **error);
+gint            gnome_rr_output_get_backlight      (GnomeRROutput         *output);
 gboolean        gnome_rr_output_set_backlight      (GnomeRROutput         *output,
                                                     gint                   value,
                                                     GError                **error);
@@ -171,7 +151,6 @@ gboolean        gnome_rr_output_set_backlight      (GnomeRROutput         *outpu
 GnomeRRCrtc **  gnome_rr_output_get_possible_crtcs (GnomeRROutput         *output);
 GnomeRRMode *   gnome_rr_output_get_current_mode   (GnomeRROutput         *output);
 GnomeRRCrtc *   gnome_rr_output_get_crtc           (GnomeRROutput         *output);
-const char *    gnome_rr_output_get_connector_type (GnomeRROutput         *output);
 gboolean        gnome_rr_output_is_builtin_display (GnomeRROutput         *output);
 void            gnome_rr_output_get_position       (GnomeRROutput         *output,
 						    int                   *x,
@@ -193,15 +172,6 @@ int             gnome_rr_mode_get_freq             (GnomeRRMode           *mode)
 /* GnomeRRCrtc */
 guint32         gnome_rr_crtc_get_id               (GnomeRRCrtc           *crtc);
 
-gboolean        gnome_rr_crtc_set_config_with_time (GnomeRRCrtc           *crtc,
-						    guint32                timestamp,
-						    int                    x,
-						    int                    y,
-						    GnomeRRMode           *mode,
-						    GnomeRRRotation        rotation,
-						    GnomeRROutput        **outputs,
-						    int                    n_outputs,
-						    GError               **error);
 gboolean        gnome_rr_crtc_can_drive_output     (GnomeRRCrtc           *crtc,
 						    GnomeRROutput         *output);
 GnomeRRMode *   gnome_rr_crtc_get_current_mode     (GnomeRRCrtc           *crtc);
@@ -213,6 +183,7 @@ GnomeRRRotation gnome_rr_crtc_get_rotations        (GnomeRRCrtc           *crtc)
 gboolean        gnome_rr_crtc_supports_rotation    (GnomeRRCrtc           *crtc,
 						    GnomeRRRotation        rotation);
 
+#if 0 /* gamma support */
 gboolean        gnome_rr_crtc_get_gamma            (GnomeRRCrtc           *crtc,
 						    int                   *size,
 						    unsigned short       **red,
@@ -223,4 +194,6 @@ void            gnome_rr_crtc_set_gamma            (GnomeRRCrtc           *crtc,
 						    unsigned short        *red,
 						    unsigned short        *green,
 						    unsigned short        *blue);
+#endif
+
 #endif /* GNOME_RR_H */

libgnome-desktop/Makefile.am
@@ -29,7 +29,6 @@ introspection_sources = 		\
 	gnome-bg.c			\
 	gnome-bg-slide-show.c		\
 	gnome-bg-crossfade.c		\
-	display-name.c			\
 	gnome-rr.c			\
 	gnome-rr-config.c		\
 	gnome-rr-output-info.c		\
@@ -42,12 +41,25 @@ introspection_sources = 		\
 
 libgnome_desktop_3_la_SOURCES = 	\
 	$(introspection_sources)	\
+	$(dbus_xrandr_built_sources)	\
 	gnome-datetime-source.h		\
 	gnome-datetime-source.c		\
 	gnome-rr-private.h		\
 	default-input-sources.h		\
 	edid.h				\
-	locarchive.h
+	locarchive.h			\
+	meta-xrandr-shared.h
+
+dbus_xrandr_built_sources = meta-dbus-xrandr.c meta-dbus-xrandr.h
+
+$(dbus_xrandr_built_sources) : Makefile.am xrandr.xml
+	$(AM_V_GEN)gdbus-codegen							\
+		--interface-prefix org.gnome.Mutter					\
+		--c-namespace MetaDBus							\
+		--generate-c-code meta-dbus-xrandr					\
+		xrandr.xml
+
+BUILT_SOURCES = $(dbus_xrandr_built_sources)
 
 libgnome_desktop_3_la_LIBADD =	\
 	$(XLIB_LIBS)		\

libgnome-desktop/display-name.c
@@ -1,144 +1,0 @@
-/*
- * Copyright 2007 Red Hat, Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * on the rights to use, copy, modify, merge, publish, distribute, sub
- * license, and/or sell copies of the Software, and to permit persons to whom
- * the Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice (including the next
- * paragraph) shall be included in all copies or substantial portions of the
- * Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.  IN NO EVENT SHALL
- * THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
-/* Author: Soren Sandmann <sandmann@redhat.com> */
-
-#include <config.h>
-#include <glib/gi18n-lib.h>
-#include <stdlib.h>
-#include <math.h>
-#include <stdio.h>
-#include <string.h>
-#include <glib.h>
-
-#include "gnome-pnp-ids.h"
-#include "edid.h"
-
-static const char *
-find_vendor (const char *code)
-{
-    const char *vendor_name;
-    GnomePnpIds *pnp_ids;
-
-    pnp_ids = gnome_pnp_ids_new ();
-    vendor_name = gnome_pnp_ids_get_pnp_id (pnp_ids, code);
-    g_object_unref (pnp_ids);
-
-    if (vendor_name)
-        return vendor_name;
-
-    return code;
-}
-
-static const double known_diagonals[] = {
-    12.1,
-    13.3,
-    15.6
-};
-
-static char *
-diagonal_to_str (double d)
-{
-    int i;
-
-    for (i = 0; i < G_N_ELEMENTS (known_diagonals); i++)
-    {
-        double delta;
-
-        delta = fabs(known_diagonals[i] - d);
-        if (delta < 0.1)
-            return g_strdup_printf ("%0.1lf\"", known_diagonals[i]);
-    }
-
-    return g_strdup_printf ("%d\"", (int) (d + 0.5));
-}
-
-char *
-make_display_size_string (int width_mm,
-                          int height_mm)
-{
-  char *inches = NULL;
-
-  if (width_mm > 0 && height_mm > 0)
-    {
-      double d = sqrt (width_mm * width_mm + height_mm * height_mm);
-
-      inches = diagonal_to_str (d / 25.4);
-    }
-
-  return inches;
-}
-
-char *
-make_display_name (const MonitorInfo *info)
-{
-    const char *vendor;
-    int width_mm, height_mm;
-    char *inches, *ret;
-
-    if (info)
-    {
-	vendor = find_vendor (info->manufacturer_code);
-    }
-    else
-    {
-        /* Translators: "Unknown" here is used to identify a monitor for which
-         * we don't know the vendor. When a vendor is known, the name of the
-         * vendor is used. */
-	vendor = C_("Monitor vendor", "Unknown");
-    }
-
-    if (info && info->width_mm != -1 && info->height_mm)
-    {
-	width_mm = info->width_mm;
-	height_mm = info->height_mm;
-    }
-    else if (info && info->n_detailed_timings)
-    {
-	width_mm = info->detailed_timings[0].width_mm;
-	height_mm = info->detailed_timings[0].height_mm;
-    }
-    else
-    {
-	width_mm = -1;
-	height_mm = -1;
-    }
-    
-    if (width_mm != -1 && height_mm != -1)
-    {
-	double d = sqrt (width_mm * width_mm + height_mm * height_mm);
-
-	inches = diagonal_to_str (d / 25.4);
-    }
-    else
-    {
-	inches = NULL;
-    }
-
-    if (!inches)
-	return g_strdup (vendor);
-
-    ret = g_strdup_printf ("%s %s", vendor, inches);
-    g_free (inches);
-
-    return ret;
-}

libgnome-desktop/edid.h
@@ -189,7 +189,5 @@ struct MonitorInfo
 };
 
 MonitorInfo *decode_edid (const uchar *data);
-char *make_display_name (const MonitorInfo *info);
-char *make_display_size_string (int width_mm, int height_mm);
 
 #endif

libgnome-desktop/gnome-rr-config.h
@@ -75,9 +75,9 @@ GnomeRRRotation gnome_rr_output_info_get_rotation (GnomeRROutputInfo *self);
 void            gnome_rr_output_info_set_rotation (GnomeRROutputInfo *self, GnomeRRRotation rotation);
 
 gboolean gnome_rr_output_info_is_connected     (GnomeRROutputInfo *self);
-void     gnome_rr_output_info_get_vendor       (GnomeRROutputInfo *self, gchar* vendor);
-guint    gnome_rr_output_info_get_product      (GnomeRROutputInfo *self);
-guint    gnome_rr_output_info_get_serial       (GnomeRROutputInfo *self);
+const char *gnome_rr_output_info_get_vendor    (GnomeRROutputInfo *self);
+const char *gnome_rr_output_info_get_product   (GnomeRROutputInfo *self);
+const char *gnome_rr_output_info_get_serial    (GnomeRROutputInfo *self);
 double   gnome_rr_output_info_get_aspect_ratio (GnomeRROutputInfo *self);
 char    *gnome_rr_output_info_get_display_name (GnomeRROutputInfo *self);
 
@@ -115,31 +115,21 @@ GType               gnome_rr_config_get_type     (void);
 
 GnomeRRConfig      *gnome_rr_config_new_current  (GnomeRRScreen  *screen,
 						  GError        **error);
-GnomeRRConfig      *gnome_rr_config_new_stored   (GnomeRRScreen  *screen,
+gboolean            gnome_rr_config_load_current (GnomeRRConfig  *self,
 						  GError        **error);
-gboolean                gnome_rr_config_load_current (GnomeRRConfig  *self,
-						      GError        **error);
-gboolean                gnome_rr_config_load_filename (GnomeRRConfig  *self,
-						       const gchar    *filename,
-						       GError        **error);
 gboolean            gnome_rr_config_match        (GnomeRRConfig  *config1,
 						  GnomeRRConfig  *config2);
 gboolean            gnome_rr_config_equal	 (GnomeRRConfig  *config1,
 						  GnomeRRConfig  *config2);
-gboolean            gnome_rr_config_save         (GnomeRRConfig  *configuration,
-						  GError        **error);
 void                gnome_rr_config_sanitize     (GnomeRRConfig  *configuration);
 gboolean            gnome_rr_config_ensure_primary (GnomeRRConfig  *configuration);
 
-gboolean	    gnome_rr_config_apply_with_time (GnomeRRConfig  *configuration,
-						     GnomeRRScreen  *screen,
-						     guint32         timestamp,
-						     GError        **error);
-
-gboolean            gnome_rr_config_apply_from_filename_with_time (GnomeRRScreen  *screen,
-								   const char     *filename,
-								   guint32         timestamp,
-								   GError        **error);
+gboolean	    gnome_rr_config_apply  (GnomeRRConfig  *configuration,
+					    GnomeRRScreen  *screen,
+					    GError        **error);
+gboolean	    gnome_rr_config_apply_persistent  (GnomeRRConfig  *configuration,
+						       GnomeRRScreen  *screen,
+						       GError        **error);
 
 gboolean            gnome_rr_config_applicable   (GnomeRRConfig  *configuration,
 						  GnomeRRScreen  *screen,
@@ -148,8 +138,5 @@ gboolean            gnome_rr_config_applicable   (GnomeRRConfig  *configuration,
 gboolean            gnome_rr_config_get_clone    (GnomeRRConfig  *configuration);
 void                gnome_rr_config_set_clone    (GnomeRRConfig  *configuration, gboolean clone);
 GnomeRROutputInfo **gnome_rr_config_get_outputs  (GnomeRRConfig  *configuration);
-
-char *gnome_rr_config_get_backup_filename (void);
-char *gnome_rr_config_get_intended_filename (void);
 
 #endif

libgnome-desktop/gnome-rr-debug.c
@@ -83,7 +83,7 @@ main (int argc, char *argv[])
 	outputs = gnome_rr_screen_list_outputs (screen);
 	for (i = 0; outputs[i] != NULL; i++) {
 		g_print ("[%s]\n", gnome_rr_output_get_name (outputs[i]));
-		g_print ("\tconnected: %i\n", gnome_rr_output_is_connected (outputs[i]));
+		g_print ("\tconnected: %i\n", 1);
 		g_print ("\tbuilt-in: %i\n", gnome_rr_output_is_builtin_display (outputs[i]));
 		g_print ("\tprimary: %i\n", gnome_rr_output_get_is_primary (outputs[i]));
 		g_print ("\tid: %i\n", gnome_rr_output_get_id (outputs[i]));

commit 1de2a6d57c2dcf7e5f6570f61ece21e8a57e958f

gnome-rr: Treat Panel as a builtin display connector
HP ZBook 17 G5 still reports "Panel" as its builtin display.
The value is overlooked when gnome-rr switched to use Mutter DBus API.

Also remove unused GNOME_RR_CONNECTOR_TYPE_PANEL.

Fixes: 632dffb7887c8650154670ae0e76822ae11e22f3

libgnome-desktop/gnome-rr.c
@@ -1688,7 +1688,8 @@ _gnome_rr_output_name_is_builtin_display (const char *name)
 	strstr (name, "Lvds") ||
 	strstr (name, "LCD")  ||  /* ... but fglrx uses "LCD" in some versions.  Shoot me now, kthxbye. */
 	strstr (name, "eDP")  ||    /* eDP is for internal built-in panel connections */
-	strstr (name, "DSI"))
+	strstr (name, "DSI")  ||
+	strstr (name, "Panel"))
         return TRUE;
 
     return FALSE;

libgnome-desktop/gnome-rr.h
@@ -85,8 +85,6 @@ typedef enum {
     GNOME_RR_ERROR_NO_DPMS_EXTENSION,	/* DPMS extension is not present */
 } GnomeRRError;
 
-#define GNOME_RR_CONNECTOR_TYPE_PANEL "Panel"  /* This is a built-in LCD */
-
 #define GNOME_TYPE_RR_SCREEN                  (gnome_rr_screen_get_type())
 #define GNOME_RR_SCREEN(obj)                  (G_TYPE_CHECK_INSTANCE_CAST ((obj), GNOME_TYPE_RR_SCREEN, GnomeRRScreen))
 #define GNOME_IS_RR_SCREEN(obj)               (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GNOME_TYPE_RR_SCREEN))