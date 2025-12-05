c53a81692bbb4405c7594576eb112e1434f1f8d0
Marco Trevisan (Treviño) <mail@3v1n0.net>
2025-12-05 08:03:39 +0100

    gnome-qr: Update license to GPL-3+
    Part of this code was already licensed as GPL3+, so let's just use this
    version and adjust the copyright holders

--- libgnome-desktop/gnome-qr/gnome-qr.h
+++ libgnome-desktop/gnome-qr/gnome-qr.h
@@ -1,24 +1,26 @@
 /* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
 /*
+ * Copyright 2019 Purism SPC
  * Copyright 2024-2025 Red Hat, Inc
  * Copyright 2024-2025 Canonical Ltd
  *
- * SPDX-License-Identifier: LGPL-2.1-or-later
+ * SPDX-License-Identifier: GPL-3.0-or-later
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General
- * Public License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Author: Ray Strode <rstrode@redhat.com>
+ * Author: Mohammed Sadiq <sadiq@sadiqpk.org>
+ *         Ray Strode <rstrode@redhat.com>
  *         Marco Trevisan <marco.trevisan@canonical.com>
  *         Joan Torres Lopez <joantolo@redhat.com>
  */
